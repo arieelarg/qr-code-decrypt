e64d6f422ed8598dc542abe6f301b0e885dcda76
Ariel Silva <arielsilvainfo@gmail.com>
2021-12-15 01:26:39 -0300

    refactor: Separate ep between types of code

--- routes/decryption.js
+++ routes/decryption.js
@@ -1,5 +1,4 @@
-const { Router } = require("express");
-const router = Router();
+const router = require("express").Router();
 
 const { fromPath } = require("pdf2pic");
 const fs = require("fs");
@@ -8,73 +7,72 @@ const PNG = require("pngjs").PNG;
 
 const Quagga = require("quagga").default;
 
-const PAGE = 1;
+const savePath = `./public/uploads`;
 
-router.get("/", async (req, res, next) => {
+router.get("/decrpyt/qrcode", async (req, res, next) => {
   try {
+    const PAGE = 1;
     const filename = "sample.pdf";
     const saveFilename = "sample";
     const options = {
       density: 100,
       saveFilename,
-      savePath: "./public/uploads",
       format: "png",
       width: 2000,
       height: 2000,
     };
 
-    const pathToPDF = `${options.savePath}/${filename}`;
+    const pathToPDF = `${savePath}/${filename}`;
 
-    const storeAsImage = fromPath(pathToPDF, options);
+    const storeAsImage = fromPath(pathToPDF, { options, ...savePath });
 
     await storeAsImage(PAGE);
 
-    console.log("Se guardo OK!");
-
-    const fileToPNG = `${options.savePath}/${saveFilename}.${PAGE}.png`;
-
-    console.log("fileToPNG: ", fileToPNG);
+    const fileToPNG = `${savePath}/${saveFilename}.${PAGE}.png`;
 
     const imgData = PNG.sync.read(fs.readFileSync(fileToPNG));
 
-    const qrResult = zbarScan(imgData);
-
-    if (qrResult) {
-      console.log("QR DETECTED!");
-    } else {
-      console.log("QR NOT DETECTED!");
-    }
-
-    try {
-      Quagga.decodeSingle(
-        {
-          src: `${options.savePath}/barcodeExample.png`,
-          numOfWorkers: 0,
-          decoder: {
-            readers: ["code_128_reader", "ean_reader"],
-          },
-        },
-        function (result) {
-          const barcodeResult = result.codeResult;
-          if (barcodeResult) {
-            console.log("BARCODE DETECTED!: ", barcodeResult.code);
-          } else {
-            console.log("BARCODE NOT DETECTED");
-          }
-        }
-      );
-
-      console.log("TERMINO y MANDA");
-
-      res.send(qrResult);
-    } catch (error) {
-      console.log("BARCODE ERROR");
-    }
+    const qrResult = !!imgData ? zbarScan(imgData) : "Error";
 
     res.send({ qrResult });
     next;
   } catch (err) {
-    console.log("ERROR: ", err);
+    console.log("QR code error: ", err);
+  }
+});
+
+router.get("/decrpyt/barcode", async (req, res, next) => {
+  try {
+    const file = "barcodeExample.png";
+    const decodeBarcode = (fileName) =>
+      new Promise((resolve, reject) => {
+        Quagga.decodeSingle(
+          {
+            src: `${savePath}/${fileName}`,
+            numOfWorkers: 0,
+            decoder: {
+              readers: ["code_128_reader", "ean_reader"],
+            },
+          },
+          (result) => {
+            const barcodeResult = result.codeResult;
+            if (barcodeResult) {
+              console.log("BARCODE DETECTED");
+              resolve(barcodeResult);
+            } else {
+              console.log("BARCODE NOT DETECTED");
+              reject("Error");
+            }
+          }
+        );
+      });
+
+    const barcodeResult = await decodeBarcode(file);
+
+    res.send({ barcodeResult: barcodeResult.code });
+    next;
+  } catch (err) {
+    console.log("Barcode error: ", err);
   }
 });
 
