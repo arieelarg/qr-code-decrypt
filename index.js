b7e7223645687a926b3b805eca24153e9130f3f0
Ariel Silva <arielsilvainfo@gmail.com>
2021-12-16 09:51:24 -0300

    feat: Run prettier on index

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 dotenv.config();
-const app = express();
-const logger = require("morgan");
+const logger = require('morgan');
 
-const decryption = require("./routes/decryption");
+const app = express();
+
+const decryption = require('./routes/decryption');
 const port = process.env.PORT || 3000;
-const host = "0.0.0.0";
+const host = '0.0.0.0';
 
 app.use(express.json());
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-app.use("/", decryption);
+app.use('/', decryption);
 
 app.listen(port, host, () => console.log(`Listening on port: ${port}`));
