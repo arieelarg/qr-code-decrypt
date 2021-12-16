97858a56c351db67eeacb89e4c45ef9033de44e8
Ariel Silva <arielsilvainfo@gmail.com>
2021-12-16 09:52:11 -0300

    refactor: Refactor endpoint to upload img and use fileHandler methods

--- routes/decryption.js
+++ routes/decryption.js
@@ -1,79 +1,110 @@
-const router = require("express").Router();
-
-const { fromPath } = require("pdf2pic");
-const fs = require("fs");
-const zbarScan = require("zbar-qr");
-const PNG = require("pngjs").PNG;
-
-const Quagga = require("quagga").default;
-
+const router = require('express').Router();
 const savePath = `./public/uploads`;
 
-router.get("/decrpyt/qrcode", async (req, res, next) => {
+const multer = require('multer');
+const upload = multer({ dest: savePath });
+const fileHandler = require('../utils/fileHandler');
+
+const zbarScan = require('zbar-qr');
+// const Quagga = require('quagga').default;
+
+const qrCodeDecrypt = async (req, res) => {
+  const { save, convertPDFtoPNG, generatePNGBuffer } = fileHandler;
+
   try {
-    const PAGE = 1;
-    const filename = "sample.pdf";
-    const saveFilename = "sample";
-    const options = {
-      density: 100,
-      saveFilename,
-      format: "png",
-      width: 2000,
-      height: 2000,
-    };
+    const originalName = await save(req.file);
 
-    const pathToPDF = `${savePath}/${filename}`;
+    const [fileName, extension] = originalName.split('.');
 
-    const storeAsImage = fromPath(pathToPDF, { options, ...savePath });
-
-    await storeAsImage(PAGE);
-
-    const fileToPNG = `${savePath}/${saveFilename}.${PAGE}.png`;
-
-    const imgData = PNG.sync.read(fs.readFileSync(fileToPNG));
-
-    const qrResult = !!imgData ? zbarScan(imgData) : "Error";
-
-    res.send({ qrResult });
-    next;
-  } catch (err) {
-    console.log("QR code error: ", err);
+    // Extension should always be PDF since the endpoint is not ready to process other file type
+    if (fileName && extension === 'pdf') {
+      // First convert PDF to PNG
+      console.log('Comienza a convertir PDF a PNG');
+      await convertPDFtoPNG(fileName, extension);
+      console.log('Termino de guardar imagen');
+      console.log('Termino de convertir PDF a PNG');
+      // Last decode QR from PNG
+      console.log('Comienza a decodificar QR');
+      const imgData = await generatePNGBuffer(fileName);
+      const qrResult = !!imgData ? zbarScan(imgData) : 'Error';
+      console.log('Termino de decodificar QR');
+      return res.json({ qrResult });
+    } else {
+      return res.json({ error: true, message: 'Ocurrió un error' });
+    }
+  } catch (e) {
+    // console.log(e);
+    return res.json({ error: true, message: 'Ocurrió un error' });
   }
-});
+};
 
-router.get("/decrpyt/barcode", async (req, res, next) => {
-  try {
-    const file = "barcodeExample.png";
-    const decodeBarcode = (fileName) =>
-      new Promise((resolve, reject) => {
-        Quagga.decodeSingle(
-          {
-            src: `${savePath}/${fileName}`,
-            numOfWorkers: 0,
-            decoder: {
-              readers: ["code_128_reader", "ean_reader"],
-            },
-          },
-          (result) => {
-            const barcodeResult = result.codeResult;
-            if (barcodeResult) {
-              console.log("BARCODE DETECTED");
-              resolve(barcodeResult);
-            } else {
-              console.log("BARCODE NOT DETECTED");
-              reject("Error");
-            }
-          }
-        );
-      });
+// router.get('/decrpyt/qrcode', async (req, res, next) => {
+//   try {
+//     const PAGE = 1;
+//     const filename = 'sample.pdf';
+//     const saveFilename = 'sample';
+//     const options = {
+//       density: 100,
+//       saveFilename,
+//       savePath: './public/uploads',
+//       format: 'png',
+//       width: 2000,
+//       height: 2000
+//     };
 
-    const barcodeResult = await decodeBarcode(file);
+//     const pathToPDF = `${options.savePath}/${filename}`;
 
-    res.send({ barcodeResult: barcodeResult.code });
-    next;
-  } catch (err) {
-    console.log("Barcode error: ", err);
-  }
-});
+//     const storeAsImage = fromPath(pathToPDF, options);
+
+//     await storeAsImage(PAGE);
+
+//     const fileToPNG = `${options.savePath}/${saveFilename}.${PAGE}.png`;
+
+//     const imgData = PNG.sync.read(fs.readFileSync(fileToPNG));
+
+//     const qrResult = !!imgData ? zbarScan(imgData) : 'Error';
+
+//     res.send({ qrResult });
+//     next;
+//   } catch (err) {
+//     console.log('Qrcode error: ', err);
+//   }
+// });
+
+// router.get('/decrpyt/barcode', async (req, res, next) => {
+//   try {
+//     const file = 'barcodeExample.png';
+//     const decodeBarcode = (fileName) =>
+//       new Promise((resolve, reject) => {
+//         Quagga.decodeSingle(
+//           {
+//             src: `${savePath}/${fileName}`,
+//             numOfWorkers: 0,
+//             decoder: {
+//               readers: ['code_128_reader', 'ean_reader']
+//             }
+//           },
+//           (result) => {
+//             const barcodeResult = result.codeResult;
+//             if (barcodeResult) {
+//               console.log('BARCODE DETECTED');
+//               resolve(barcodeResult);
+//             } else {
+//               console.log('BARCODE NOT DETECTED');
+//               reject('Error');
+//             }
+//           }
+//         );
+//       });
+
+//     const barcodeResult = await decodeBarcode(file);
+
+//     return res.send({ barcodeResult: barcodeResult.code });
+//   } catch (err) {
+//     console.log('Barcode error: ', err);
+//   }
+// });
+
+router.post('/decrypt/qrcode', upload.single('invoice'), qrCodeDecrypt);
 
 module.exports = router;
